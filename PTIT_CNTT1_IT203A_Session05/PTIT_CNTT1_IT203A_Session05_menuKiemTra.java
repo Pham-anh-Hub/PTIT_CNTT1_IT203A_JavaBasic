6ceccb3f93e4c109905e72e166f2e14bf22e3454
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2026-01-29 09:26:16 +0700

    Add files via upload

--- PTIT_CNTT1_IT203A_Session05/PTIT_CNTT1_IT203A_Session05_menuKiemTra.java
+++ PTIT_CNTT1_IT203A_Session05/PTIT_CNTT1_IT203A_Session05_menuKiemTra.java
@@ -32,6 +32,21 @@ public class PTIT_CNTT1_IT203A_Session05_menuKiemTra {
         }
     }
 
+    public static int searchStudentId (String[] ids,int size, String target){
+        String regex = "[a-zA-Z0-9]{7}";
+        if (target.length() < 7 && target.matches(regex)){
+            for (int i=0; i<size; i++){
+                if(ids[i].toLowerCase().contains(target.toLowerCase().trim())){
+                    return i;
+                }
+            }
+
+        }else{
+            System.out.println("Chuỗi nhập vào không hợp lệ");
+        }
+        return -1;
+    }
+
 
 
 
@@ -56,6 +71,7 @@ public class PTIT_CNTT1_IT203A_Session05_menuKiemTra {
                 case 1:
                     // In ra danh sách MSSV hiện có
                     if(size > 0){
+                        System.out.println("Danh sách sinh viên: ");
                         displayList(studentIds, size);
                     }else{
                         System.out.println("Danh sách sinh viên trống.");
@@ -63,7 +79,7 @@ public class PTIT_CNTT1_IT203A_Session05_menuKiemTra {
                     break;
                 case 2:
                     // Thêm mới (Có Regex): * Yêu cầu nhập MSSV mới
-
+                    sc.nextLine();
                     System.out.print("Nhập mã sinh viên mới: ");
                     String newStudentId = sc.nextLine();
                     if(newStudentId.equalsIgnoreCase("")){
@@ -76,17 +92,40 @@ public class PTIT_CNTT1_IT203A_Session05_menuKiemTra {
                     // Nhập vị trí (index) cần sửa, kiểm tra tính hợp lệ của index cho phép nhập MSSV mới (cũng phải thỏa mãn Regex)
                     System.out.print("Nhập vị trí cần cập nhật: ");
                     int index = sc.nextInt();
+                    sc.nextLine();
                     if(index>=0 && index<size){
                         System.out.print("Nhập mã sinh viên cập nhật: ");
                         String updateId = sc.nextLine();
-                        if()
+                        if(!updateId.trim().equals("")){
+                            updateStudentId(studentIds, updateId, index-1);
+                        }
                     }
                     break;
                 case 4:
+                    System.out.print("Nhập vị trí cần xóa: ");
+                    int deleteIdx = sc.nextInt() - 1;
+                    if (deleteIdx >= 0 && deleteIdx < size) {
+                        for (int i = deleteIdx; i < size - 1; i++) {
+                            studentIds[i] = studentIds[i + 1];
+                        }
+                        size--;
+                        System.out.println("Xóa thành công!");
+                    }
                     break;
                 case 5:
+                    // Tìm kiếm (Regex)
+                    sc.nextLine();
+                    System.out.print("Nhập chuỗi mã sinh viên cần tìm: ");
+                    String searchString = sc.nextLine();
+                    int targetIndex = searchStudentId(studentIds, size, searchString);
+                    if(targetIndex != -1){
+                        System.out.println("Mã sinh viên cần tìm ở vị trí có chỉ số: " + targetIndex);
+                    }else{
+                        System.out.println("Không tìm thấy mã sinh viên");
+                    }
                     break;
                 case 6:
+                    System.out.println("Thoát chương trình!!");
                     break;
                 default:
                     System.out.println("Lựa chọn không hợp lệ!");
