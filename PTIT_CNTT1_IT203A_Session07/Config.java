d0aed2dad68ff08530f72d06a627887c4f46d6ac
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2026-02-05 11:49:54 +0700

    clean project

--- PTIT_CNTT1_IT203A_Session07/Config.java
+++
@@ -1,9 +0,0 @@
-package PTIT_CNTT1_IT203A_Session07;
-
-public class Config {
-    public static final int MAX_SCORE = 999999999;
-    public static final int MIN_SCORE = 0;
-
-    public Config() {
-    }
-}
--- PTIT_CNTT1_IT203A_Session09/Baitap6/Shape.java
+++
@@ -1,8 +0,0 @@
-package PTIT_CNTT1_IT203A_Session09.Baitap6;
-
-public class Shape {
-
-    public double area(){
-        return 0;
-    }
-}
